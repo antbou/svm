585a555cbed47e697d719ef435369679fc6e6ae3
antho <bouillant.anthony@gmail.com>
2025-07-23 15:29:37 +0200

    Initial commit

---
+++ src/main/java/sv/menu/svm/SvmApplication.java
@@ -0,0 +1,13 @@
+package sv.menu.svm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvmApplication.class, args);
+	}
+
+}
