5ab9870087738a75013c3c5654310fa4bf8f8db1
antho <bouillant.anthony@gmail.com>
2025-07-24 19:07:31 +0200

    Add hot reload

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,6 @@
 set -e
 
 # Launch Spring Boot app in development mode
-mvn spring-boot:run
+exec mvn spring-boot:run \
+  -Dspring.devtools.restart.enabled=true \
+  -Dspring.devtools.livereload.enabled=true
