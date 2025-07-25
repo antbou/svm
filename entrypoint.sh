107e98ccc9c5c8962c9558bfb18ca9b8b862f9d1
antho <bouillant.anthony@gmail.com>
2025-07-25 23:36:57 +0200

    Refactor application.properties

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -e
 
-# Launch Spring Boot app in development mode
-exec mvn spring-boot:run \
-  -Dspring.devtools.restart.enabled=true \
-  -Dspring.devtools.livereload.enabled=true
+SPRING_PROFILE=${SPRING_PROFILES_ACTIVE:-local}
+
+echo "Launching Spring Boot with profile: $SPRING_PROFILE"
+
+if [ "$SPRING_PROFILE" = "local" ]; then
+  exec mvn spring-boot:run \
+    -Dspring-boot.run.profiles=$SPRING_PROFILE \
+    -Dspring.devtools.restart.enabled=true \
+    -Dspring.devtools.livereload.enabled=true
+else
+  exec mvn spring-boot:run \
+    -Dspring-boot.run.profiles=$SPRING_PROFILE
+fi
